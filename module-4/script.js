111c6782f1810696e4c3a269cc233848a822a24c
sindhulahari <67785524+sindhulahari@users.noreply.github.com>
2020-07-31 15:19:52 +0530

    Create script.js

---
+++ module-4/script.js
@@ -0,0 +1,14 @@
+var names = [ "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+
+ 	var letter= names[i].charAt(0).toLowerCase();
+
+ 	if (letter === "j") {
+
+ 		saygoodbye();
+ 	}
+
+ 	else {
+ 		sayhello();
+ 	}
+ }
