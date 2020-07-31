c7415ad5d84dce044b88749a98b89b411ec2a801
sindhulahari <67785524+sindhulahari@users.noreply.github.com>
2020-07-31 14:41:30 +0530

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -1,4 +1,4 @@
-var names = [ "john", "bob", "joy","edison","sam"];
+var names = [ "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 
  	var letter= names[i].charAt(0).toLowerCase();
